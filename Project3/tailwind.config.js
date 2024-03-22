86fdd5a471da444ac96dba85070da415a950bf6a
FuzzilFox144 <125853450+CN-LEON-DX@users.noreply.github.com>
2024-03-22 20:26:33 +0700

    First home page
    22/3/2024

---
+++ Project3/tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+      screens: {
+        'sm': '576px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1240px',                                    
+        '2xl': '1240px',                              
+      },
+      extend: {
+        colors: {
+          primary: '#000000',
+          secondary: '#F57F20'
+        },
+        fontFamily: {
+          roboto:  ['Be Vietnam Pro', 'sans-serif'],
+          //robotoCo: ['Roboto Condensed', 'sans-serif']
+        }
+      }
+    }
+  }
