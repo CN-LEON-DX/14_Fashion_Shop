0b758a78854cab20a69f09a132d2cdc6de8fa829
FuzzilFox144 <125853450+CN-LEON-DX@users.noreply.github.com>
2024-05-16 18:05:51 +0700

    Edit some animation

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// AOS
+AOS.init();
+// End AOS
+const swiperSectionComment = new Swiper(`.swiper-section-comment`, {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+    },
+});
+
